c4ece02a2daecf035491e8a63080ef4bd9f45acb
Gary Boyer <gboyer.dev@gmail.com>
2023-11-26 18:37:24 -0500

    Finish dashboard functionality

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,7 +1,26 @@
-import { initTRPC } from '@trpc/server'
+import { TRPCError, initTRPC } from '@trpc/server'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 const t = initTRPC.create()
+const middleware = t.middleware
+
+const isAuth = middleware(async (opts) => {
+	const { getUser } = getKindeServerSession()
+	const user = await getUser()
+
+	if (!user || !user.id) {
+		throw new TRPCError({ code: 'UNAUTHORIZED' })
+	}
+
+	return opts.next({
+		ctx: {
+			userId: user.id,
+			user,
+		},
+	})
+})
 
 // Base router and procedure helpers
 export const router = t.router
 export const publicProcedure = t.procedure
+export const privateProcedre = t.procedure.use(isAuth)
